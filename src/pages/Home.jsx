42515660a47fd4e02832192de9e3995956428f77
Lonzieeee <wanderilorna@gmail.com>
2025-05-10 14:54:46 +0300

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,28 @@
+import { ThemeToggle } from "../components/ThemeToggle"
+import { StarBackground } from "../components/StarBackground"
+
+export const Home = () => {
+    return <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
+{/* Theme toogle */}
+
+<ThemeToggle />
+
+{/* Background */}
+
+<StarBackground />
+
+
+
+{/* Navbar */}
+
+
+
+{/* Main content */}
+
+
+{/* Footer */}
+
+
+
+    </div>
+}
