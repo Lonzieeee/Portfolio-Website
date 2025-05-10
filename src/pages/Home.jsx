26ab0638055ac5ac2e9ef7ead79fd9628e8c9f88
Lonzieeee <wanderilorna@gmail.com>
2025-05-10 20:25:57 +0300

    5th

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,6 @@ import { StarBackground } from "../components/StarBackground"
 import { HeroSection } from "../components/HeroSection"
 import { AboutSection } from "../components/AboutSection"
 import { SkillsSection } from "../components/SkillsSection"
-import { ProjectsSection } from "../components/ProjectsSection"
 
 export const Home = () => {
     return <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
@@ -21,7 +20,6 @@ export const Home = () => {
     <HeroSection />
     <AboutSection />
     <SkillsSection />
-    <ProjectsSection />
 </main>
 
 
